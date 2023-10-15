73465f28167dc49d84fd003d0d9ab00bf4323f8d
barti132 <barteksredzinski@gmail.com>
2023-10-15 18:24:48 +0200

    init commit

---
+++ src/test/java/pl/hosannaponglish/dictionaryservice/DictionaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hosannaponglish.dictionaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryServiceApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
